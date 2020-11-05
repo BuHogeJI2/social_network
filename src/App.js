e79c0ce89967d025ac8b9517511f7ea7a58ae904
BuHogeJI <dmitrydemidovich@yanex.ru>
2020-11-05 22:26:59 +0300

    add BrowserRouter and Route modules

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className="wrapper">
-            <Header/>
-            <Sidebar/>
-            <div className='wrapper-content'>
-                <Profile/>
-                {/*<Dialogs/>*/}
+        <BrowserRouter>
+            <div className="wrapper">
+                <Header/>
+                <Sidebar/>
+                <div className='wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/messages' component={Dialogs}/>
+                    <Route path='/news' component={Dialogs}/>
+                    <Route path='/music' component={Dialogs}/>
+                    <Route path='/settings' component={Dialogs}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     )
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,30 @@
+import React from 'react'
 import css from './Sidebar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Sidebar = () => {
 	return (
 		<nav className="nav">
-			<div className={css.item}>
+			<NavLink to='/profile' className={css.item} activeClassName={css.active}>
 				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbbWNIEz16l8oSPuCp39LHI869mMzDBJadnQ&usqp=CAU" alt="profile"/>
 				<div className={css.text_block}><span>Profile</span></div>
-			</div>
-			<div className={css.item}>
+			</NavLink>
+			<NavLink to='/messages' className={css.item}>
 				<img src="https://readtextmessages.net/wp-content/uploads/2014/09/best-phone-to-read-text-messages-300x248.jpg" alt="message"/>
 				<div className={css.text_block}><span>Message</span></div>
-			</div>
-			<div className={css.item}>
+			</NavLink>
+			<NavLink to='/news' className={css.item}>
 				<img src="https://lh3.googleusercontent.com/2I4ZgNCk5eyTu6l9OiCp1WL1STxMeG4Nszg_WMqLQY_YUzfMsGbCyVVx52cOrm_SbDzNQwXBbzT3sDPiONfq=s1500-pp" alt=""/>
 				<div className={css.text_block}><span>News</span></div>
-			</div>
-			<div className={css.item}>
+			</NavLink>
+			<NavLink to='/music' className={css.item}>
 				<img src="https://wek.ru/webp/images/stalo-izvestno-kakuyu-muzyku-neobxodimo-slushat-dlya-povysheniya-samoocenki.jpg" alt="music"/>
 				<div className={css.text_block}><span>Music</span></div>
-			</div>
-			<div className={css.item}>
+			</NavLink>
+			<NavLink to='/settings' className={css.item}>
 				<img src="https://aussiebornandbred.com.au/wp-content/uploads/2019/06/Trent-3-900x900.jpg" alt="settings"/>
 				<div className={css.text_block}><span>Settings</span></div>
-			</div>
+			</NavLink>
 		</nav>
 	)
 }
