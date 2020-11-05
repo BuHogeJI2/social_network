3dc16dfc8effabcba14be233191fb42115d6ba2d
BuHogeJI <dmitrydemidovich@yanex.ru>
2020-11-05 23:03:49 +0300

    add few dialogs and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,19 @@ import css from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={css.dialogs}>
+            <div className={css.dialog_items}>
+                <div className={css.dialog}>Alesya</div>
+                <div className={css.dialog}>Nikita</div>
+                <div className={css.dialog}>Vlad</div>
+                <div className={css.dialog}>Andrew</div>
+                <div className={css.dialog}>IT-kamasutra</div>
+            </div>
+            <div className={css.messages}>
+                <div className={css.message}>Hi!</div>
+                <div className={css.message}>When to the gym?</div>
+                <div className={css.message}>Where is cities???</div>
+            </div>
         </div>
     )
 }
