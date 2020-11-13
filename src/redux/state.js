7299fe3fbdea6a8302dacc51bc815775dba5ce8f
BuHogeJI <dmitrydemidovich@yanex.ru>
2020-11-14 02:05:56 +0300

    move data to redux/state.js

---
+++ src/redux/state.js
@@ -0,0 +1,26 @@
+let state = {
+    profilePage: {
+        posts: [
+            {text: 'Why your code is too bad?', id: 1},
+            {text: 'You have to work harder', id: 2},
+            {text: 'Dont be a lazy peace of shit', id: 3},
+            {text: 'Go WORK!', id: 4},
+        ],
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Alesya'},
+            {id: 2, name: 'Nikita'},
+            {id: 3, name: 'Vlad'},
+            {id: 4, name: 'Andrew'},
+            {id: 5, name: 'IT-Kamasutra'},
+        ],
+        messages: [
+            {id: 1, message: 'Hi!'},
+            {id: 1, message: 'When to the gym?'},
+            {id: 1, message: 'Where is cities???'},
+        ],
+    },
+}
+
+export default state
